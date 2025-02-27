4e3545b4041578e1f5f71faa5153ca4c11cf5d9f
Leeyerin0210 <lyl8661@naver.com>
2025-02-27 15:53:49 +0900

    feat : 댓글, 하이퍼링크 기능 완료

--- js/script.js
+++ js/script.js
@@ -24,3 +24,38 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const commentForm = document.querySelector(".commentInput");
+    const commentInput = commentForm.querySelector("input[type='text']");
+    const commentList = document.querySelector(".comment-list");
+
+    commentForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const commentText = commentInput.value.trim();
+
+        if (commentText === "") {
+            alert("댓글을 입력해주세요.");
+            return;
+        }
+
+        const commentItem = document.createElement("li");
+        commentItem.innerHTML = `
+      <div class="comment-item">
+        <div class="comment-author">
+          <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+          <span>방문자</span>
+        </div>
+        <div class="comment-content">${commentText}</div>
+      </div>
+    `;
+
+        commentList.appendChild(commentItem);
+        commentInput.value = "";
+        alert("댓글이 입력되었습니다.");
+    });
+
+    commentForm.addEventListener("reset", function () {
+        commentInput.value = "";
+    });
+});
